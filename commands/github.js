dce402ad9473aacb7917454b94e78299adb6621f
nomfundokagwe <nomfundonene0507@gmail.com>
2025-08-03 11:01:03 +0200

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 async function githubCommand(sock, chatId, message) {
   try {
-    const res = await fetch('https://api.github.com/repos/reddragon-xmd/---');
+    const res = await fetch('https://api.github.com/repos/nomfundokagwe/Explore-md-V1');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
